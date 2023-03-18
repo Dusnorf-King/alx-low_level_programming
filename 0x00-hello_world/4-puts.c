9d7d0340761fcad4cf6cf162064e3d9caa9d3049
Dusnorf-King <thevintageplugng@gmail.com>
2023-03-18 10:38:34 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts function
- *
- * Return: 0
-*/
+
+/**
+ * main - pricipal function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
