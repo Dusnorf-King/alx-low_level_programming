c6bd1790c6c0f73ea4ce253dfa7492ab6e111db2
Dusnorf-King <thevintageplugng@gmail.com>
2023-03-27 14:48:57 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void _puts(char *str);
-void print_rev(char *s)
+void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
